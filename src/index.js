11a8defa4883978939d0f23f78abc21317083373
Jake Siddall <jake.siddall@gmail.com>
2020-03-27 12:37:11 -0700

    Add configuration for VSCode Debugger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 //const http = require('http');
 import http from 'http'; // ES6 syntax
 const requestHandler = function (req, res) {
+  if (req.method === 'POST' && req.url === '/users') {
+    res.statusCode = 400;
+    res.end();
+    return;
+  }
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello, World!');
 }
