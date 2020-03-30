2040b2925d1a2764a5864e5c5cf8b590b76c553e
Jake Siddall <jake.siddall@gmail.com>
2020-03-30 14:22:14 -0700

    Move common logic into middleware functions

--- src/index.js
+++ src/index.js
@@ -4,23 +4,48 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: 1e6}));
+function checkEmptyPayload(req, res, next) {
+  if (
+    ['POST', 'PATCH', 'PUT'].includes(req.method)
+    && req.headers['content-length'] === '0'
+  ) {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json({
+      message: 'Payload should not be empty',
+    });
+  }
+  next();
+}
 
-app.post('/users', (req, res) => {
-      if (req.headers['content-length'] === '0') {
-        res.status(400);
-        res.set('Content-Type', 'application/json');
-        res.json({ message: 'Payload should not be empty' }); 
-        return;
-      }
-      if (req.headers['content-type'] !== 'application/json') {
-        res.writeHead(415, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({
-          message: 'The "Content-Type" header must always be "application/json"',
-        }));
-        return;
-      }
-});
+function checkContentTypeIsSet(req, res, next) {
+  if (
+    req.headers['content-length']
+    && req.headers['content-length'] !== '0'
+    && !req.headers['content-type']
+  ) {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json({ message: 'The "Content-Type" header must be set for requests with a non-empty payload' });
+  }
+  next();
+}
+
+function checkContentTypeIsJson(req, res, next) {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(415);
+    res.set('Content-Type', 'application/json');
+    res.json({ message: 'The "Content-Type" header must always be "application/json"' });
+  }
+  next();
+}
+
+app.use(checkEmptyPayload);
+app.use(checkContentTypeIsSet);
+app.use(checkContentTypeIsJson);
+app.use(bodyParser.json({ limit: 1e6 }));
+
+app.post('/users', (req, res, next) => { next(); });
 
 app.use((err, req, res, next) => {
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err && err.type === 'entity.parse.failed') {
