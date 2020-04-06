8cecabd54283966411334bb326e71865f65e51bd
Jake Siddall <jake.siddall@gmail.com>
2020-04-06 14:51:56 -0700

    Make standalone E2E test script

--- scripts/e2e.test.sh
+++ scripts/e2e.test.sh
@@ -1,27 +1,18 @@
 #!/usr/bin/env bash
-
-# Make sure the port is not already bound
 if ss -lnt | grep -q :$SERVER_PORT; then
   echo "Another process is already listening to port $SERVER_PORT"
   exit 1;
 fi
-
 RETRY_INTERVAL=${RETRY_INTERVAL:-0.2}
-if ! systemctl --quiet is-active elasticsearch.service; then
+if ! systemctl is-active --quiet elasticsearch.service; then
   sudo systemctl start elasticsearch.service
-  # Wait until Elasticsearch is ready to respond
   until curl --silent $ELASTICSEARCH_HOSTNAME:$ELASTICSEARCH_PORT -w "" -o /dev/null; do
     sleep $RETRY_INTERVAL
   done
 fi
-
-# Run our API server as a background process
 yarn run serve &
-RETRY_INTERVAL=0.2
 until ss -lnt | grep -q :$SERVER_PORT; do
   sleep $RETRY_INTERVAL
 done
-# Run test
 npx cucumber-js spec/cucumber/features --require-module @babel/register --require spec/cucumber/steps
-# Terminate all processes within the same process group by sending a SIGTERM signal
 kill -15 0
