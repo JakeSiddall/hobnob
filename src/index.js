d03e27ad9d196d1ea69b51fc927cc8e50a80bdf5
Jake Siddall <jake.siddall@gmail.com>
2020-04-07 14:34:40 -0700

    Extract request handlers into modules

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import checkEmptyPayload from './middleware/check-empty-payload';
 import checkContentTypeIsSet from './middleware/check-content-type-is-set';
 import checkContentTypeIsJson from './middleware/check-content-type-is-json';
 import errorHandler from './middleware/error-handler';
+import createUser from './handlers/users/create';
+import injectionHandlerDependencies from './utils/inject-handler-dependencies';
 
 const client = new elasticsearch.Client({
   host: `${process.env.ELASTICSEARCH_PROTOCOL}://${process.env.ELASTICSEARCH_HOSTNAME}:${process.env.ELASTICSEARCH_PORT}`,
@@ -18,44 +20,7 @@ app.use(checkContentTypeIsSet);
 app.use(checkContentTypeIsJson);
 app.use(bodyParser.json({ limit: 1e6 }));
 
-app.post('/users', (req, res, next) => {
-  if (
-    !Object.prototype.hasOwnProperty.call(req.body, 'email') || !Object.prototype.hasOwnProperty.call(req.body, 'password')
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'Payload must contain at least the email and password fields' });
-  }
-  if (
-    typeof req.body.email !== 'string'
-    || typeof req.body.password !== 'string'
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'The email and password fields must be of type string' });
-    return;
-  }
-  if (!/^[\w.+]+@\w+\.\w+$/.test(req.body.email)) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'The email field must be a valid email.' });
-    return;
-  }
-  client.index({
-    index: process.env.ELASTICSEARCH_INDEX,
-    type: 'user',
-    body: req.body,
-  }).then((result) => {
-    res.status(201);
-    res.set('Content-Type', 'text/plain');
-    res.send(result._id);
-  }).catch(() => {
-    res.status(500);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'Internal Server Error' });
-  });
-  //next();
-});
+app.post('/users', injectionHandlerDependencies(createUser, client));
 
 app.use(errorHandler);
 
