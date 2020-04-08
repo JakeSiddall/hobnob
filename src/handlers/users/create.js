9e206b7f58d926abc579627a4d0eca04d76a861c
Jake Siddall <jake.siddall@gmail.com>
2020-04-08 14:01:04 -0700

    Fully validate Create User request payload

--- src/handlers/users/create.js
+++ src/handlers/users/create.js
@@ -2,21 +2,21 @@ import ValidationError from '../../validators/errors/validation-error';
 import create from '../../engines/users/create';
 
 function createUser(req, res, db) {
-  create(req,db).then((result) => {
+  create(req, db).then((result) => {
     res.status(201);
     res.set('Content-Type', 'text/plain');
-    res.send(result._id);
+    return res.send(result._id);
   }, (err) => {
     if (err instanceof ValidationError) {
       res.status(400);
       res.set('Content-Type', 'application/json');
-      return res.json({message: err.message });
+      return res.json({ message: err.message });
     }
     return undefined;
   }).catch(() => {
     res.status(500);
     res.set('Content-Type', 'application/json');
-    return res.json({message: 'Internal Servor Error' });
+    return res.json({ message: 'Internal Server Error' });
   });
 }
 
