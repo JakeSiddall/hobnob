2819ecc3297c91b77a10c2c8d725d6bf68a4c9ab
Jake Siddall <jake.siddall@gmail.com>
2020-04-10 16:09:56 -0700

    Add unit tests for checkEmptyPayload middleware (2)

---
+++ src/middleware/check-content-type-is-json/index.unit.test.js
@@ -0,0 +1,9 @@
+import assert, { AssertionError } from 'assert';
+import checkContentTypeIsJson from '.';
+import deepEqual from 'lodash.isequal';
+import deepClone from 'lodash.clonedeep';
+import { spy, stub } from 'sinon';
+
+describe('checkContentTypeIsJson', function() {
+    
+})
--- src/middleware/check-empty-payload/index.js
+++ src/middleware/check-empty-payload/index.js
@@ -5,14 +5,11 @@ function checkEmptyPayload(req, res, next) {
     ) {
       res.status(400);
       res.set('Content-Type', 'application/json');
-      res.json({
+      return res.json({
         message: 'Payload should not be empty',
       });
     }
-    else {
-      next();
-    }
-    //next();
+    return next();
   }
-
-  export default checkEmptyPayload;
+  
+export default checkEmptyPayload;
