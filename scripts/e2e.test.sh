807c47bfb7c301385d9f97450d598e8f70383f9c
Jake Siddall <jake.siddall@gmail.com>
2020-05-07 11:12:50 -0700

    Debug after last push to remote dev branch

--- scripts/e2e.test.sh
+++ scripts/e2e.test.sh
@@ -1,29 +1,16 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-=======
-
-# Make sure the port is not already bound
->>>>>>> ec939164bbe2d30e1063c3b586f2456b29e38522
 if ss -lnt | grep -q :$SERVER_PORT; then
   echo "Another process is already listening to port $SERVER_PORT"
   exit 1;
 fi
-<<<<<<< HEAD
+
 RETRY_INTERVAL=${RETRY_INTERVAL:-0.2}
 if ! systemctl is-active --quiet elasticsearch.service; then
   sudo systemctl start elasticsearch.service
-=======
-
-RETRY_INTERVAL=${RETRY_INTERVAL:-0.2}
-if ! systemctl --quiet is-active elasticsearch.service; then
-  sudo systemctl start elasticsearch.service
-  # Wait until Elasticsearch is ready to respond
->>>>>>> ec939164bbe2d30e1063c3b586f2456b29e38522
   until curl --silent $ELASTICSEARCH_HOSTNAME:$ELASTICSEARCH_PORT -w "" -o /dev/null; do
     sleep $RETRY_INTERVAL
   done
 fi
-<<<<<<< HEAD
 # Clean the test index (if it exists)
 curl --silent -o /dev/null -X DELETE "$ELASTICSEARCH_HOSTNAME:$ELASTICSEARCH_PORT/$ELASTICSEARCH_INDEX"
 
@@ -32,16 +19,4 @@ until ss -lnt | grep -q :$SERVER_PORT; do
   sleep $RETRY_INTERVAL
 done
 npx cucumber-js spec/cucumber/features --require-module @babel/register --require spec/cucumber/steps
-=======
-
-# Run our API server as a background process
-yarn run serve &
-RETRY_INTERVAL=0.2
-until ss -lnt | grep -q :$SERVER_PORT; do
-  sleep $RETRY_INTERVAL
-done
-# Run test
-npx cucumber-js spec/cucumber/features --require-module @babel/register --require spec/cucumber/steps
-# Terminate all processes within the same process group by sending a SIGTERM signal
->>>>>>> ec939164bbe2d30e1063c3b586f2456b29e38522
 kill -15 0
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import '@babel/polyfill';
 import express from 'express';
 import bodyParser from 'body-parser';
 import elasticsearch from 'elasticsearch';
-<<<<<<< HEAD
 import checkEmptyPayload from './middleware/check-empty-payload';
 import checkContentTypeIsSet from './middleware/check-content-type-is-set';
 import checkContentTypeIsJson from './middleware/check-content-type-is-json';
@@ -12,8 +11,6 @@ import ValidationError from './validators/errors/validation-error';
 import createUserHandler from './handlers/users/create';
 import createUserEngine from './engines/users/create';
 import createUserValidator from './validators/users/create';
-=======
->>>>>>> ec939164bbe2d30e1063c3b586f2456b29e38522
 
 const client = new elasticsearch.Client({
   host: `${process.env.ELASTICSEARCH_PROTOCOL}://${process.env.ELASTICSEARCH_HOSTNAME}:${process.env.ELASTICSEARCH_PORT}`,
@@ -21,7 +18,6 @@ const client = new elasticsearch.Client({
 
 const app = express();
 
-<<<<<<< HEAD
 const handlerToEngineMap = new Map([
   [createUserHandler, createUserEngine],
 ]);
@@ -29,103 +25,15 @@ const handlerToEngineMap = new Map([
 const handlerToValidatorMap = new Map([
   [createUserHandler, createUserValidator],
 ]);
-=======
-function checkEmptyPayload(req, res, next) {
-  if (
-    ['POST', 'PATCH', 'PUT'].includes(req.method)
-    && req.headers['content-length'] === '0'
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'Payload should not be empty',
-    });
-  }
-  next();
-}
-
-function checkContentTypeIsSet(req, res, next) {
-  if (
-    req.headers['content-length']
-    && req.headers['content-length'] !== '0'
-    && !req.headers['content-type']
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'The "Content-Type" header must be set for requests with a non-empty payload' });
-  }
-  next();
-}
-
-function checkContentTypeIsJson(req, res, next) {
-  if (!req.headers['content-type'].includes('application/json')) {
-    res.status(415);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'The "Content-Type" header must always be "application/json"' });
-  }
-  next();
-}
->>>>>>> ec939164bbe2d30e1063c3b586f2456b29e38522
 
 app.use(checkEmptyPayload);
 app.use(checkContentTypeIsSet);
 app.use(checkContentTypeIsJson);
 app.use(bodyParser.json({ limit: 1e6 }));
 
-<<<<<<< HEAD
 app.post('/users', injectionHandlerDependencies(createUserHandler, client, handlerToEngineMap, handlerToValidatorMap, ValidationError));
 
 app.use(errorHandler);
-=======
-app.post('/users', (req, res, next) => {
-  if (
-    !Object.prototype.hasOwnProperty.call(req.body, 'email') || !Object.prototype.hasOwnProperty.call(req.body, 'password')
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'Payload must contain at least the email and password fields' });
-  }
-  if (
-    typeof req.body.email !== 'string'
-    || typeof req.body.password !== 'string'
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'The email and password fields must be of type string' });
-    return;
-  }
-  if (!/^[\w.+]+@\w+\.\w+$/.test(req.body.email)) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'The email field must be a valid email.' });
-    return;
-  }
-  client.index({
-    index: process.env.ELASTICSEARCH_INDEX,
-    type: 'user',
-    body: req.body,
-  }).then((result) => {
-    res.status(201);
-    res.set('Content-Type', 'text/plain');
-    res.send(result._id);
-  }).catch(() => {
-    res.status(500);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'Internal Server Error' });
-  });
-  //next();
-});
-
-app.use((err, req, res, next) => {
-  if (err instanceof SyntaxError && err.status === 400 && 'body' in err && err.type === 'entity.parse.failed') {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'Payload should be in JSON format'})
-    return;
-  }
-  next();
-});
->>>>>>> ec939164bbe2d30e1063c3b586f2456b29e38522
 
 app.listen(process.env.SERVER_PORT, () => {
   // eslint-disable-next-line no-console
